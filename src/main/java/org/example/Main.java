f9ef1be995c001e20b18d8c8a9db566d300f92ba
GRAVDO <42653608+gravdo12345@users.noreply.github.com>
2023-12-22 20:09:45 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,26 @@ public class Main {
         System.out.print("Write N: ");
         int n = scanner.nextInt();
 
+
+        //Ітераційний метод:
+        //Часова складність: O(n)
+        //Просторова складність: O(1)
         System.out.println("Iter:");
         IterFibonacci iterFibonacci = new IterFibonacci(n);
         System.out.println(iterFibonacci);
 
+
+        //РРекурсивний метод:
+        //Часова складність: O(2n)
+        //Просторова складність: O(n)
         System.out.println("Recursive: ");
         RecFibonacci recFibonacci = new RecFibonacci(n);
         System.out.println(recFibonacci);
 
+
+        //Метод динамічного програмування:
+        //Часова складність: O(n)
+        //Просторова складність: O(n)
         System.out.println("Dynamic: ");
         DynamicFibonacci dynamicFibonacci = new DynamicFibonacci(n);
         System.out.println(dynamicFibonacci);
